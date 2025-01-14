b2c6bcaeca326d8d2790f96e6b37e02786a08c44
Yash Kumar Rana <2200520100072@ietlucknow.ac.in>
2025-01-14 18:02:37 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express, { urlencoded } from "express";
+import userRoute from "./routes/user.route.js";
+import connectDb from "./utils/db.js";
+
+dotenv.config({}); 
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
+
+const corsOptions = {
+    origin: process.env.URL, 
+    credentials: true,
+};
+app.use(cors(corsOptions));
+// api bnani h yha pe
+app.use("/api/v1/user",userRoute);
+
+// Routes
+app.get("/", (req, res) => {
+    return res.status(200).json({
+        message: "I am coming from backend",
+        success: true,
+    });
+});
+
+// Server and Database Connection
+
+app.listen(PORT, () => {
+   connectDb();
+    console.log(`Server listening at http://localhost:${PORT}`);
+});
