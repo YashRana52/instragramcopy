522bde69a3197f8e11909ee198f8508a9416c001
Yash Kumar Rana <2200520100072@ietlucknow.ac.in>
2025-02-03 18:30:50 +0530

    "edit add"

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,85 +1,92 @@
-import { useEffect, useMemo } from "react";
-import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from 'react'
+import ChatPage from './components/ChatPage'
+import EditProfile from './components/EditProfile'
+import Home from './components/Home'
+import Login from './components/Login'
+import MainLayout from './components/MainLayout'
+import Profile from './components/Profile'
+import Signup from './components/Signup'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { io } from "socket.io-client";
-import { setSocket } from "./redux/socketSlice";
-import { setOnlineUsers } from "./redux/chatSlice";
-import { setLikeNotification } from "./redux/rtnSlice";
-import ProtectedRoutes from "./components/ProtectedRoutes";
-import MainLayout from "./components/MainLayout";
-import Profile from "./components/Profile";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
-import Home from "./components/Home";
+import { useDispatch, useSelector } from 'react-redux'
+import { setSocket } from './redux/socketSlice'
+import { setOnlineUsers } from './redux/chatSlice'
+import { setLikeNotification } from './redux/rtnSlice'
+import ProtectedRoutes from './components/ProtectedRoutes'
+
+
+const browserRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <ProtectedRoutes><MainLayout /></ProtectedRoutes>,
+    children: [
+      {
+        path: '/',
+        element: <ProtectedRoutes><Home /></ProtectedRoutes>
+      },
+      {
+        path: '/profile/:id',
+        element: <ProtectedRoutes> <Profile /></ProtectedRoutes>
+      },
+      {
+        path: '/account/edit',
+        element: <ProtectedRoutes><EditProfile /></ProtectedRoutes>
+      },
+      {
+        path: '/chat',
+        element: <ProtectedRoutes><ChatPage /></ProtectedRoutes>
+      },
+    ]
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
+  },
+])
 
 function App() {
-  const { user } = useSelector((store) => store.auth); // Get the logged-in user
-  const { socket } = useSelector((store) => store.socketio);
+  const { user } = useSelector(store => store.auth);
+  const { socket } = useSelector(store => store.socketio);
   const dispatch = useDispatch();
 
-  // Create socket connection only when the user is logged in
   useEffect(() => {
     if (user) {
-      const socketio = io("http://localhost:5000", {
-        query: { userId: user?._id },
-        transports: ["websocket"],
+      const socketio = io('http://localhost:5000', {
+        query: {
+          userId: user?._id
+        },
+        transports: ['websocket']
       });
       dispatch(setSocket(socketio));
 
-      socketio.on("getOnlineUsers", (onlineUsers) => {
+      // listen all the events
+      socketio.on('getOnlineUsers', (onlineUsers) => {
         dispatch(setOnlineUsers(onlineUsers));
       });
 
-      socketio.on("notification", (notification) => {
+      socketio.on('notification', (notification) => {
         dispatch(setLikeNotification(notification));
       });
 
       return () => {
-        socketio.disconnect();
+        socketio.close();
         dispatch(setSocket(null));
-      };
+      }
     } else if (socket) {
-      socket.disconnect();
+      socket.close();
       dispatch(setSocket(null));
     }
   }, [user, dispatch]);
 
-  // Routes logic
-  const browserRouter = useMemo(
-    () =>
-      createBrowserRouter([
-        {
-          path: "/",
-          element: user ? (
-            <MainLayout /> // Show main layout if user is logged in
-          ) : (
-            <Navigate to="/login" replace /> // Redirect to login if not logged in
-          ),
-          children: [
-            { path: "/", element: <Home /> },
-            {
-              path: "/profile/:id",
-              element: user ? (
-                <Profile /> // Show profile if user is logged in
-              ) : (
-                <Navigate to="/login" replace />
-              ),
-            },
-          ],
-        },
-        {
-          path: "/login",
-          element: user ? <Navigate to="/" replace /> : <Login />, // Redirect to home if already logged in
-        },
-        {
-          path: "/signup",
-          element: user ? <Navigate to="/" replace /> : <Signup />, // Redirect to home if already logged in
-        },
-      ]),
-    [user]
-  );
-
-  return <RouterProvider router={browserRouter} />;
+  return (
+    <>
+      <RouterProvider router={browserRouter} />
+    </>
+  )
 }
 
-export default App;
+export default App
