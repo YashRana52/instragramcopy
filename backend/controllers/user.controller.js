af976c84dcfe9dfcb92ef722d7997f16947cceb1
Yash Kumar Rana <2200520100072@ietlucknow.ac.in>
2025-07-17 09:38:14 +0530

    cookie-update

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -80,7 +80,8 @@ export const login = async (req, res) => {
             following: user.following,
             posts: populatedPosts
         }
-        return res.cookie('token', token, { httpOnly: true, sameSite: 'strict', maxAge: 1 * 24 * 60 * 60 * 1000 }).json({
+        return res.cookie('token', token, { httpOnly: true, secure: true,           
+  sameSite: 'None', maxAge: 1 * 24 * 60 * 60 * 1000 }).json({
             message: `Welcome back ${user.username}`,
             success: true,
             user
